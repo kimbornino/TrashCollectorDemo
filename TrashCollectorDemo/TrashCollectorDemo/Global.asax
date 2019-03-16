b9842d854a067fcc63492a956946091f278baeff
Kimbornino <kimbornino@gmail.com>
2019-03-16 15:21:58 -0500

    Init commit

---
+++ TrashCollectorDemo/TrashCollectorDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TrashCollectorDemo.MvcApplication" Language="C#" %>
---
+++ TrashCollectorDemo/TrashCollectorDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrashCollectorDemo.Startup))]
+namespace TrashCollectorDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
