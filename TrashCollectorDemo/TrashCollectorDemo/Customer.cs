e75480b3ffcf5e4a4cd0cc9a69fc958753f41a6b
Kimbornino <kimbornino@gmail.com>
2019-03-16 15:28:25 -0500

    Add the customer model to include ID, Name, Address, Zip, and MonthlyBill.  Several of these fields will be FK references for other models.  ZIP will be an FK on Pickups as well as customerID.

---
+++ TrashCollectorDemo/TrashCollectorDemo/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollectorDemo
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Zip { get; set; }
+
+        public double MonthlyBill { get; set; }
+
+    }
+}
